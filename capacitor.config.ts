e3d0930f6ce96075fea7a167c44a8832d1082512
colesm <colesmatheo@outlook.fr>
2026-03-13 11:05:58 +0100

    chore: final build and assets sync

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,22 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.nexus.app',
   appName: 'Nexus',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#0d0d0d",
+      androidScaleType: "CENTER",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      spinnerColor: "#c2185b",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
